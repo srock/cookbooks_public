facb71c0acb6d381c380c6148dcf73f961cedb5a
srock <shane@rightscale.com>
2011-08-24 16:25:10 -0600

    Edited cookbooks/hello_world/recipes/default.rb via GitHub

--- cookbooks/hello_world/recipes/default.rb
+++ cookbooks/hello_world/recipes/default.rb
@@ -1,20 +1,12 @@
-#
-# Cookbook Name:: hello_world
-# Recipe:: default
-#
-# Copyright 2011, Chris Fordham
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-Chef::Log.info("Hello World!")
-Chef::Log.info("From Shane's Repo!")
+log "IP is #{node[:ipaddress]}"
+
+right_link_tag "web:active=true"
+right_link_tag "web:ip=#{node[:ipaddress]}"
+
+server_collection "active_login_servers" do
+  tags "rs_login:state=active"
+end
+
+node[:server_collection].each do |tag|
+  log "Server tag: #{tag}"
+end
